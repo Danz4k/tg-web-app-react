ef9e1c18b00d116cb0ffe024200020b585a0b56c
Danz4k <bota.danya@mail.ru>
2024-09-03 21:23:30 +0300

    initial

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import Button from '../Button';
 import { useTelegram } from '../../hooks/useTelegram';
 
-const Header = (props) => {
-    const {user, onClose} = useTelegram()
+const Header = () => {
+    const {user, onClose} = useTelegram();
 
     return (
         <div className='header'>
